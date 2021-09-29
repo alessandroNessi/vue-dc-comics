1d03f3d5439e7da6950936751215f942aa5f3a78
alessandroNessi <alessandronessi@msn.com>
2021-09-29 17:09:29 +0200

    header part static done

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
   <div id="app">
-    <Header/>
-    <Main/>
-    <Footer/>
+    <body>  
+      <Header/>
+      <Main/>
+      <Footer/>
+    </body>
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
-import Main from "@/components/Main.vue";
-import Footer from "@/components/Footer.vue";
+  import Header from "@/components/Header.vue";
+  import Main from "@/components/Main.vue";
+  import Footer from "@/components/Footer.vue";
 
-export default {
-  name: "App",
-  components: {
-    Header,
-    Main,
-    Footer
-  },
-};
+  export default {
+    name: "App",
+    components: {
+      Header,
+      Main,
+      Footer
+    },
+  };
 </script>
 
 <style lang="scss">
-#app {
-  @import url('https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&display=swap');
-  font-family: 'PT Sans Narrow', sans-serif;
-}
+  @import "./assets/style/common.scss";
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,69 @@
 <template>
-  <h1>header</h1>
+  <div class="container">
+    <div class="logo__container">
+      <a href="" class="logo">
+        <img src="../assets/img/dc-logo.png" alt="dc logo">
+      </a>
+    </div>
+    <nav>
+      <ul class="options">
+        <li class="option"><a href="">CIPPA</a></li>
+        <li class="option">CIPPA</li>
+        <li class="option">LIPPA12345646</li>
+        <li class="option">CIPPA</li>
+        <li class="option">CIPPA</li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Header",
-
-  // props: {
-  //   msg: String,
-  // },
 };
 </script>
 
 <style scoped lang="scss">
+  // variabili
+  @import "../assets/style/common.scss";
+  $height: 150px;
+  .selected{
+    border-bottom: 5px solid $main__blue;
+    color: $main__blue!important;
+  }
+  .container{
+    height: $height;
+    display: flex;
+    .logo__container, nav{
+      width: 50%;
+    }
+    .logo__container{
+      display: flex;
+      justify-content: center;
+      .logo{
+        padding: 30px;
+        width: $height;
+        img{
+          width: 100%;
+        }
+      }
+    }
+    nav{
+      .options{
+        height: 100%;
+        display: flex;
+        .option{
+          a{
+            font-weight: 700;
+            color: inherit;
+          }
+          color:$main__gray;
+          height: 100%;
+          line-height: $height;
+          margin-right: 50px;
+        }
+      }
+    }
+  } 
 
 </style>
